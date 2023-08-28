1ba9265ed1c08a4ee461257d4ae33cd20f3f31a6
lg <leonardoagustingimenez@gmail.com>
2023-08-28 16:25:46 -0300

    Esta todo hecho salvo por: JSON, almacenamiento local, opcional

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
+const listaContactos = [];
 
-    const elementoNombre = document.getElementById("nombre");
-    const elementoTelefono = document.getElementById("telefono");
-    const elementoLista = document.getElementById("lista");
-    const listaContactos = [];
-    
-    function mostrarLista(lista){
-        elementoLista.innerHTML = "";
-        lista.forEach(contacto => {
-            const li = document.createElement("li");
-            li.innerHTML = `${contacto.nombre}: ${contacto.telefono} <button id="eliminarContacto">Eliminar</button>`;
+document.addEventListener('DOMContentLoaded', () => {
+    const elementoNombre = document.getElementById('nombre');
+    const elementoTelefono = document.getElementById('telefono');
+    const elementoLista = document.getElementById('lista');
+
+    function mostrarLista(lista) {
+        elementoLista.innerHTML = '';
+        lista.forEach((contacto, index) => {
+            const li = document.createElement('li');
+            li.innerHTML = `${contacto.nombre}: ${contacto.telefono} <button class='eliminarContacto' data-index=${index}>Eliminar</button>`;
             elementoLista.appendChild(li);
         });
     }
 
-    document.getElementById("ingresarDatos").addEventListener("submit", (e) => {
+    document.getElementById('ingresarDatos').addEventListener('submit', (e) => {
         e.preventDefault();
-        const contacto = {nombre: elementoNombre.value, telefono: elementoTelefono.value};
-        elementoNombre.value = "";
-        elementoTelefono.value = "";
+        const contacto = { nombre: elementoNombre.value, telefono: elementoTelefono.value };
+        elementoNombre.value = '';
+        elementoTelefono.value = '';
         listaContactos.push(contacto);
-        mostrarLista(listaContactos);
-       
-        document.getElementById("eliminarContacto").addEventListener("click", (e) => {
-            //Array.from(e.target.parentNode.children).indexOf(e.target);
-            console.log(Array.from(e.target.parentNode.children).indexOf(e.target));
-        })
+        mostrarLista(listaContactos.sort((a, b) => a.nombre.localeCompare(b.nombre, undefined, { sensitivity: 'base' })));
+    })
+
+    elementoLista.addEventListener('click', (e) => {
+        if (e.target.classList.contains('eliminarContacto')) {
+            listaContactos.splice(e.target.getAttribute('data-index'), 1);
+            mostrarLista(listaContactos);
+        }
     })
 })
-
-
-
-
