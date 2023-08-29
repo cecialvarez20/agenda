4875a443756e90bf4bbb219e69626008a5c3e5f9
lg <leonardoagustingimenez@gmail.com>
2023-08-29 02:09:23 -0300

    Eficiencia mejorada

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-const listaContactos = [];
-
 document.addEventListener('DOMContentLoaded', () => {
     const elementoNombre = document.getElementById('nombre');
     const elementoTelefono = document.getElementById('telefono');
     const elementoLista = document.getElementById('lista');
 
-    function mostrarLista(lista) {
-        elementoLista.innerHTML = '';
-        lista.forEach((contacto, index) => {
-            const li = document.createElement('li');
-            li.innerHTML = `${contacto.nombre}: ${contacto.telefono} <button class='eliminarContacto' data-index=${index}>Eliminar</button>`;
-            elementoLista.appendChild(li);
-        });
+    const listaContactos = [];
+
+    function insertarOrdenado(contacto) {
+        let inf = 0;
+        let sup = listaContactos.length - 1;
+        while (inf <= sup) {
+            let med = (inf + sup) >>> 1;
+            if (listaContactos[med].nombre.localeCompare(contacto.nombre, undefined, { sensitivity: 'base' }) < 0)
+                inf = med + 1;
+            else
+                sup = med - 1;
+        }
+        listaContactos.splice(inf, 0, contacto);
+        const li = document.createElement('li');
+        li.innerHTML = `${contacto.nombre}: ${contacto.telefono} <button class='eliminarContacto' data-index=${inf}>Eliminar</button>`;
+        elementoLista.insertBefore(li, elementoLista.children[inf]);
     }
 
     document.getElementById('ingresarDatos').addEventListener('submit', (e) => {
@@ -19,14 +26,13 @@ document.addEventListener('DOMContentLoaded', () => {
         const contacto = { nombre: elementoNombre.value, telefono: elementoTelefono.value };
         elementoNombre.value = '';
         elementoTelefono.value = '';
-        listaContactos.push(contacto);
-        mostrarLista(listaContactos.sort((a, b) => a.nombre.localeCompare(b.nombre, undefined, { sensitivity: 'base' })));
+        insertarOrdenado(contacto);
     })
 
     elementoLista.addEventListener('click', (e) => {
         if (e.target.classList.contains('eliminarContacto')) {
             listaContactos.splice(e.target.getAttribute('data-index'), 1);
-            mostrarLista(listaContactos);
+            e.target.parentElement.remove();
         }
     })
 })
